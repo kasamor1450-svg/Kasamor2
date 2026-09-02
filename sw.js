0e8acc090f0dff383ee8590e57b90dd123895a4a
kasamor1450-svg <kasamor1450@gmail.com>
2026-09-02 13:01:24 +0300

    Fix user login passwords and quick-select helper

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Mustagro PWA
 // Cache Version: mustagro-v5.0-lockdown (Strict Mandatory Login)
-const CACHE_NAME = 'mustagro-v5.3-clean';
+const CACHE_NAME = 'mustagro-v5.4-fixed';
 
 const PRECACHE_ASSETS = [
   './manifest.json',
